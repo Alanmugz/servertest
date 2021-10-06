54ce51532ed8467594b5555882997cf18574b756
amulligan <amulligan@continuumcommerce.com>
2021-10-06 17:18:20 +0100

    F0000 - Initail commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:13-alpine
+
+COPY package.json /data/
+WORKDIR /data/
+RUN npm install
+COPY . /data/app/
+WORKDIR /data/app/
+
+EXPOSE 3000
+CMD [ "node", "app.js" ]
---
+++ utils/database.js
@@ -0,0 +1,19 @@
+import { Sequelize } from 'sequelize';
+
+
+const sequelize = new Sequelize(LoginDB, root, 123456, {
+    host: localhost,
+    dialect: "mysql",
+    port: 3306,
+    operatorsAliases: false,
+  
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });
+
+
+export default sequelize;
