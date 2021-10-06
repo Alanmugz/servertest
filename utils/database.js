a4a7b4621487e4e14b1920a219aae0eec7e1f4d2
amulligan <amulligan@continuumcommerce.com>
2021-10-06 17:33:51 +0100

    F0000 - Updated db connections string

--- utils/database.js
+++ utils/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 
-const sequelize = new Sequelize(LoginDB, root, 123456, {
+const sequelize = new Sequelize("LoginDB", "root", 123456, {
     host: localhost,
     dialect: "mysql",
     port: 3306,
